11ee0f5a294104c142c8ed756a23a66565d257a5
stebeh <stephen.behnfeldt@assaabloy.com>
2024-11-06 13:48:16 -0500

    Lesson 2: "Your First Route and View"
    Added 2 new routes and a new Blade template.

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function () {
+    return '<h1>Hello from the About Page.</h1>';
+});
+
+Route::get('/contact', function () {
+    return view( 'contact' );
+});
