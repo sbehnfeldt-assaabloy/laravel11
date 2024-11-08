acd860972241fabe131a08eda9cab4cedbf4d734
stebeh <stephen.behnfeldt@assaabloy.com>
2024-11-08 07:26:13 -0500

    Lesson 6: "View Data and Route Wildcards"
    Passing data to a view using an associative array; each key in the array is converted to a variable in the Blade template.
    Route wildcards: part of the route is in curly braces, and the text in the brace is converted to a parameter in the function callback

---
+++ resources/views/job.blade.php
@@ -0,0 +1,13 @@
+<x-layout>
+    <x-slot:heading>
+        Job
+    </x-slot:heading>
+
+    <h2>{{ $job[ 'title' ] }}</h2>
+
+    <p>
+        This job pays {{ $job[ 'salary'] }} per year.
+    </p>
+
+
+</x-layout>
---
+++ resources/views/jobs.blade.php
@@ -0,0 +1,13 @@
+<x-layout>
+    <x-slot:heading>Jobs Page</x-slot:heading>
+
+    <ul>
+        @foreach($jobs as $job)
+            <li>
+                <a href="/jobs/{{$job[ 'id' ]}}">
+                    <strong>{{$job[ 'title']}}</strong>: Pays {{$job[ 'salary']}}
+                </a>
+            </li>
+        @endforeach
+    </ul>
+</x-layout>
